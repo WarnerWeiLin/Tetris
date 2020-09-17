3199681963822d2ee72fb8c20ec3beade667b539
Warner Lin <WarnerWeiLin@gmail.com>
2020-09-17 15:37:43 -0500

    Added Initial Controls

---
+++ src/components/controls.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+class Controls extends Component{
+  render(){
+    return(
+      <div className='controls'>
+        <button className='control-button' onClick={(e)=>{
+        }}>Left</button>
+        <button className='control-button' onClick={(e)=>{
+        }}>Right</button>
+        <button className='control-button' onClick={(e)=>{
+        }}>Rotate</button>
+        <button className='control-button' onClick={(e)=>{
+        }}>Down</button>
+      </div>
+    )
+  }
+}
